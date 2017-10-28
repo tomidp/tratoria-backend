b91273a01c807c0f588cf2e464fb75fdf7b3e8d3
Dwi Priyohutomo <dwi.priyohutomo@gdn-commerce.com>
2017-10-28 16:48:41 +0700

    add log on tratoria

--- tratoria-backend/src/main/java/com/dp/tratoria/controller/TratoriaController.java
+++ tratoria-backend/src/main/java/com/dp/tratoria/controller/TratoriaController.java
@@ -2,6 +2,8 @@ package com.dp.tratoria.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,11 +16,14 @@ import com.dp.tratoria.service.CustomerService;
 @RequestMapping("/api/tratoria")
 public class TratoriaController {
 	
+	private static final Logger log = LoggerFactory.getLogger(TratoriaController.class);
+	
 	@Autowired
 	private CustomerService customerService;
 	
     @RequestMapping(value = "/findAll", method = RequestMethod.GET)
     public List<Customer> findAll() {
+    	log.info("Tratoria Controller Find All");
         return customerService.findAll();
     }
 
